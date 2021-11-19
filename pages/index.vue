6b755d0c500f09cdaf34af4fcc974f606b1d9511
nejdetkadir <nejdetkadir.550@gmail.com>
2021-11-20 02:41:08 +0300

    Create index page of application

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template lang="pug">
-  h1 homepage
+  .p-5.mb-4.mt-5.dark-content.rounded-3
+    .container-fluid.py-5
+      h1.display-5.fw-bold.text-white Welcome to RORIES
+      p.col-md-8.fs-4.text-white Looking for the most talked about movies from the around the world? They're all on Rories. You can follow movies, writers, directors, stars and genres then the system will suggest movies based on your follows
+      nuxt-link.btn.btn-primary.btn-lg(to="/users") Sign In/Up
 </template>
 <script>
-export default {}
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'users-background'
+    }
+  }
+}
 </script>
+
+<style>
+.dark-content{
+  background-color: #000000bf;
+}
+</style>
